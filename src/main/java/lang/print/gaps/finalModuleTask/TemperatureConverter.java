3a57ea7007381d7c4d0d616b064739213942a49d
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-09 18:06:24 +0500

    hi

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,8 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        //F = (C * 9/5) + 32
-       int F = (temperatureCelsius * 9/5) + 32;
+        double F = (double) (temperatureCelsius * 9/5) + 32;
         System.out.println(F);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
